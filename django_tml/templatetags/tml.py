c811a9ef5ab110a9ed02c79c163b346559a11c33
Kamun Rustem <r.kamun@gmail.com>
2016-02-29 17:33:47 +0600

    trl template tag for labels and variables trans

--- django_tml/templatetags/tml.py
+++ django_tml/templatetags/tml.py
@@ -13,18 +13,98 @@ from django.template.defaulttags import token_kwargs
 from django.utils import six, translation
 import sys
 from django.utils.translation.trans_real import trim_whitespace
+from django.utils.safestring import SafeData, mark_safe
 from django_tml import Translation
 from tml import legacy
-from django.templatetags.i18n import BlockTranslateNode as BaseBlockTranslateNode
+from django.templatetags.i18n import BlockTranslateNode as BaseBlockTranslateNode, TranslateNode as BaseTranslateNode
 from tml.translation import Key
 from tml.dictionary import TranslationIsNotExists
 from tml.session_vars import get_current_translator
 
+
 register = Library()
 
-class BlockTranslateNode(BaseBlockTranslateNode):
 
-    def __init__(self, extra_context, singular, plural, countervar, counter, description, trimmed, nowrap = False, legacy = False):
+class TmlStringMixin(object):
+
+    def wrap_label(self, text, key, translated):
+        if self.nowrap:
+            # nowrap flag is set
+            return text
+        return self.wrap_string(text, key, translated)
+
+    def wrap_string(self, text, key, translated):
+        translator = get_current_translator()
+        if not translator or not translator.is_inline():
+            return text
+        # @todo: 1. tml_pending must handle: if ongoing to API
+        # 2. if translation came and has flag (attribute) `locked` => tml_locked
+        # 3. see ruby decorators/html.rb
+        class_name = 'tml_translated' if translated else 'tml_not_translated'
+        return six.u('<tml:label class="tml_translatable %(class_name)s" data-translation_key="%(key)s" data-target_locale="%(locale)s">%(text)s</tml:label>') % ({'key':key.key, 'text':text, 'class_name': class_name, 'locale': Translation.instance().context.locale})
+
+
+class TranslateNode(TmlStringMixin, BaseTranslateNode):
+    def __init__(self, filter_expression, nowrap = False, asvar=None, message_context=None):
+        """ Tag for {% trl %}
+            filter_expression (string): translateable string expr
+            asvar (boolean): render or store in context for reuse
+            message_context (string): key description
+            nowrap (boolean): no not wrap result in <tml:label> for inline tranlation
+        """
+        self.asvar = asvar
+        self.message_context = message_context
+        self.filter_expression = filter_expression
+        if isinstance(self.filter_expression.var, six.string_types):
+            self.filter_expression.var = Variable("'%s'" %
+                                                  self.filter_expression.var)
+        self.nowrap = nowrap
+
+    def render(self, context):
+        description = ''
+        if self.message_context:
+            description = self.message_context.resolve(context)
+        output = self.filter_expression.resolve(context)
+        value = self.translate(self.get_value(output, context), description)
+        if self.asvar:
+            context[self.asvar] = value
+            return ''
+        else:
+            return value
+
+    def get_value(self, output, context):
+        value = render_value_in_context(output, context)
+        is_safe = isinstance(value, SafeData)
+        # Restore percent signs. Percent signs in template text are doubled
+        # so they are not interpreted as string format flags.
+        value = value.replace('%%', '%')
+        return mark_safe(value) if is_safe else value
+
+    def translate(self, label, description):
+        """ Translate label
+            Args:
+                label (string): translated label
+                data (dict): user data
+                description: description
+            Returns:
+                translated text, key, translated
+        """
+        context = Translation.instance().context
+        try:
+            translation = context.fetch(label, description)
+            translated = True
+        except TranslationIsNotExists:
+            translation = context.fallback(label, description)
+            translated = False
+
+        return self.wrap_label(context.render(translation, {}, {}),
+                               translation.key,
+                               translated)
+
+
+class BlockTranslateNode(TmlStringMixin, BaseBlockTranslateNode):
+
+    def __init__(self, extra_context, singular, plural, countervar, counter, description, trimmed, nowrap = False, legacy = False, asvar=None):
         """ Block for {% tr %} {% endtr %}
             extra_context (dict): block with params
             singular (string): label for singular
@@ -45,31 +125,38 @@ class BlockTranslateNode(BaseBlockTranslateNode):
         self.trimmed = trimmed
         self.legacy = legacy
         self.nowrap = nowrap
+        self.asvar = asvar
 
 
-    def render(self, data, nested=False):
+    def render(self, context, nested=False):
         """ Render result """
         if self.description:
-            description = self.description.resolve(data)
+            description = self.description.resolve(context)
         else:
             description = ''
 
         custom_data = {}
         for var, val in list(self.extra_context.items()):
-            custom_data[var] = val.resolve(data)
-        data.update(custom_data)
+            custom_data[var] = val.resolve(context)
+        context.update(custom_data)
 
         if self.plural and self.countervar and self.counter:
             # Plural:
-            count = self.counter.resolve(data)
-            data[self.countervar] = count
+            count = self.counter.resolve(context)
+            context[self.countervar] = count
             if count == 1:
                 label, void = self.render_token_list(self.singular)
             else:
                 label, void = self.render_token_list(self.plural)
         else:
             label, void = self.render_token_list(self.singular)
-        return self.translate(label.strip(), data, description)
+
+        result = self.translate(label, context, description)
+        if self.asvar:
+            context[self.asvar] = result
+            return ''
+        else:
+            return result
 
 
     def translate(self, label, data, description):
@@ -92,22 +179,6 @@ class BlockTranslateNode(BaseBlockTranslateNode):
         return self.wrap_label(context.render(translation, data, {}),
                                translation.key,
                                translated)
-
-    def wrap_label(self, text, key, translated):
-        if self.nowrap:
-            # nowrap flag is set
-            return text
-        return self.wrap_string(text, key, translated)
-
-    def wrap_string(self, text, key, translated):
-        translator = get_current_translator()
-        if not translator or not translator.is_inline():
-            return text
-        # @todo: 1. tml_pending must handle: if ongoing to API
-        # 2. if translation came and has flag (attribute) `locked` => tml_locked
-        # 3. see ruby decorators/html.rb
-        class_name = 'tml_translated' if translated else 'tml_not_translated'
-        return six.u('<tml:label class="tml_translatable %(class_name)s" data-translation_key="%(key)s" data-target_locale="%(locale)s">%(text)s</tml:label>') % ({'key':key.key, 'text':text, 'class_name': class_name, 'locale': Translation.instance().context.locale})
 
 
 class LegacyBlockTranlationNode(BlockTranslateNode):
@@ -165,6 +236,7 @@ def do_block_translate(parser, token, legacy = False):
     close_token = 'endblocktrans' if legacy else 'endtr'
     options = {}
     remaining_bits = bits[1:]
+    asvar = None
     while remaining_bits:
         option = remaining_bits.pop(0)
         if option in options:
@@ -189,6 +261,13 @@ def do_block_translate(parser, token, legacy = False):
                     '"context" in %r tag expected '
                     'exactly one argument.') % bits[0]
                 six.reraise(TemplateSyntaxError, TemplateSyntaxError(msg), sys.exc_info()[2])
+        elif option == "asvar":
+            try:
+                value = remaining_bits.pop(0)
+            except IndexError:
+                msg = "No argument provided to the '%s' tag for the asvar option." % bits[0]
+                six.reraise(TemplateSyntaxError, TemplateSyntaxError(msg), sys.exc_info()[2])
+            asvar = value
         elif option == "trimmed":
             value = True
         elif option == "nowrap":
@@ -232,9 +311,88 @@ def do_block_translate(parser, token, legacy = False):
 
     cls = LegacyBlockTranlationNode if legacy else BlockTranslateNode
     return cls(extra_context, singular, plural, countervar,
-                              counter, message_context, trimmed = trimmed, legacy = legacy, nowrap = options.get('nowrap', False))
+                              counter, message_context, trimmed = trimmed, legacy = legacy, nowrap = options.get('nowrap', False), asvar=asvar)
 
 @register.tag("blocktrans")
 def do_block_translate_legacy(parser, token):
     return do_block_translate(parser, token, True)
 
+
+@register.tag('trl')
+def do_translate(parser, token):
+    """This will mark a string for translation and will
+    translate the string for the current language.
+
+    Usage::
+
+        {% trl "hello world" %}
+
+    This will mark the string for translation so it will
+    be handled by tml register backend and will run the string through the tml translation engine.
+
+    You can use variables instead of constant strings
+    to translate stuff you marked somewhere else::
+
+    {% trl variable %}
+
+    This will just try to translate the contents of
+    the variable ``variable``. Make sure that the string
+    in there is something that is in tml dashboard.
+
+    It is possible to store the translated string into a variable::
+
+        {% trl "this is a test" as var %}
+        {{ var }}
+
+    Contextual translations are also supported::
+
+        {% trl "this is a test" context "greeting" %}
+
+    This is equivalent to calling pgettext instead of (u)gettext."""
+    bits = token.split_contents()
+    if len(bits) < 2:
+        raise TemplateSyntaxError("'%s' takes at least one argument" % bits[0])
+    message_string = parser.compile_filter(bits[1])
+    remaining = bits[2:]
+
+    noop = False
+    asvar = None
+    message_context = None
+    seen = set()
+    invalid_context = {'as', 'nowrap'}
+
+    while remaining:
+        option = remaining.pop(0)
+        if option in seen:
+            raise TemplateSyntaxError(
+                "The '%s' option was specified more than once." % option,
+            )
+        elif option == 'context':
+            try:
+                value = remaining.pop(0)
+            except IndexError:
+                msg = "No argument provided to the '%s' tag for the context option." % bits[0]
+                six.reraise(TemplateSyntaxError, TemplateSyntaxError(msg), sys.exc_info()[2])
+            if value in invalid_context:
+                raise TemplateSyntaxError(
+                    "Invalid argument '%s' provided to the '%s' tag for the context option" % (value, bits[0]),
+                )
+            message_context = parser.compile_filter(value)
+        elif option == 'as':
+            try:
+                value = remaining.pop(0)
+            except IndexError:
+                msg = "No argument provided to the '%s' tag for the as option." % bits[0]
+                six.reraise(TemplateSyntaxError, TemplateSyntaxError(msg), sys.exc_info()[2])
+            asvar = value
+        elif option == 'nowrap':
+            pass
+        else:
+            raise TemplateSyntaxError(
+                "Unknown argument for '%s' tag: '%s'. The only options "
+                "available are 'noop', 'context' \"xxx\", and 'as VAR'." % (
+                    bits[0], option,
+                )
+            )
+        seen.add(option)
+    return TranslateNode(message_string, asvar=asvar, message_context=message_context, nowrap='nowrap' in seen)
