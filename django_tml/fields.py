c8835c04c6860f2b69ef3f3a9cc3f3b5f0175de8
Kamun Rustem <r.kamun@gmail.com>
2016-03-02 21:28:21 +0600

    trans field

---
+++ django_tml/fields.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+
+class TmlModelChoiceField(forms.ModelChoiceField):
+
+    def label_from_instance(self, obj):
+        return _(super(TransModelChoiceField, self).label_from_instance(obj))
