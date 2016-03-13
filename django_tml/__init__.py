728dcf40d5af3fee00206c019e2270ad2c56b65c
Kamun Rustem <r.kamun@gmail.com>
2016-03-13 14:48:25 +0600

    Bump version: 0.1.7 → 0.1.8

--- django_tml/__init__.py
+++ django_tml/__init__.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from tml.logger import get_logger
 
 
-__VERSION__ = '0.1.7'
+__VERSION__ = '0.1.8'
 
 
 def tr(label, data = None, description = None, options = {}):
