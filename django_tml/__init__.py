10e0b5572ef95f67e1ce77b64b06f32a83256d5f
Kamun Rustem <r.kamun@gmail.com>
2016-03-18 00:47:24 +0600

    demo upgraded

--- django_tml/__init__.py
+++ django_tml/__init__.py
@@ -4,6 +4,7 @@ from django.utils import translation
 from .translator import Translation
 from django.conf import settings
 from tml.logger import get_logger
+from tml import get_context as get_current_context, get_current_language, get_current_locale, with_block_options
 
 
 __VERSION__ = '0.1.8'
@@ -14,6 +15,21 @@ def tr(label, data = None, description = None, options = {}):
     if error:
         get_logger().exception(error)
     return value
+
+def gettext(label):
+    return ugettext(label)
+
+def ugettext(label):
+    return Translation.instance().ugettext(label)
+
+def ungettext(singular, plural, number):
+    return Translation.instance().ungettext(label)
+
+def pgettext(context, label):
+    return Translation.instance().pgettext(context, label)
+
+def npgettext(context, singular, plural, number):
+    return Translation.instance().npgettext(context, singular, plural, number)
 
 def activate(locale, dry_run=False, tml_settings=None):
     Translation.instance(tml_settings=tml_settings).activate(locale, dry_run=dry_run)
